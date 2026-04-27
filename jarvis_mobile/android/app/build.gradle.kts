722e3f6e36d137327921d198b52b2dd4454ae2f4
Ammonium2103 <kingdroom1906@gmail.com>
2026-04-27 16:11:54 +0700

    Cập nhật Standalone v3.5: Fix lỗi Registrar và đồng bộ SDK 35

--- jarvis_mobile/android/app/build.gradle.kts
+++ jarvis_mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.jarvis_mobile"
-    compileSdk = 36
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,8 +20,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.jarvis_mobile"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         
